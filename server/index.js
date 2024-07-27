b76a31e1701c74ee3e23c0759cac3c0bcaaa9b78
Patrick Murphy <patrickmurphydev@gmail.com>
2024-07-27 14:04:13 -0700

    add changes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const Ollama = require("ollama-node");
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static("public"));
 
 // SERVER STATE - Displayed on /ping
 let isDBConnected = false;
@@ -19,7 +19,7 @@ let isAIProcessing = false;
 
 // SETUP AI Instances
 const ollama = new Ollama.Ollama();
-ollama.setModel("phi3:mini");//llama3");
+ollama.setModel("llama3");//"phi3:mini");
 
 // SETUP DB Instances
 mongoose
@@ -38,7 +38,15 @@ mongoose
 // Ping Page Route
 // == Display Server State
 app.get("/ping", (_req, res) => {
-  return res.json({ msg: "Ping Successful ||  DB Connected: "+isDBConnected + " ||  AI Connected: " + isAIConnected + " ||  AI Status: " + (isAIProcessing ? "Processing..." : "Ready.")});
+  return res.json({
+    msg:
+      "Ping Successful ||  DB Connected: " +
+      isDBConnected +
+      " ||  AI Connected: " +
+      isAIConnected +
+      " ||  AI Status: " +
+      (isAIProcessing ? "Processing..." : "Ready."),
+  });
 });
 
 app.use("/api/auth", authRoutes);
@@ -48,26 +56,38 @@ const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
 
-const promptAI = async (data, sockets) => {
-  var { sendUserSocket, fromUserSocket } = sockets;
-  console.log("Send AI");``
-  const response = await ollama.generate(data.msg);
-  socket.to(sendUserSocket).emit("msg-recieve", response.output);
-  io.to(fromUserSocket).emit("msg-recieve", response.output);
-
-
-  //persist
+const storeMessage = async (msg, to, from) => {
   const Messages = require("./models/messageModel");
-  const newMsg = await Messages.create({
-    message: { text: response.output },
-    users: [data.from, data.to],
+  return await Messages.create({
+    message: { text: msg },
+    users: [from, to],
     sender: mongoose.Types.ObjectId(process.env.AIUUID),
   });
-}
+};
+
+const broadcastMsg = (sockets, msg, params) => {
+  sockets[0].to(sockets[2]).emit(msg, params);
+  sockets[1].to(sockets[3]).emit(msg, params);
+};
+
+const promptAI = async (data, sockets) => {
+  var { sendUserSocket, fromUserSocket } = sockets;
+  console.log("Send AI");
+  ``;
+  const response = await ollama.generate(data.msg);
+  broadcastMsg(
+    [socket, io, sendUserSocket, fromUserSocket],
+    "msg-recieve",
+    response.output
+  );
+
+  //persist
+  const newMsg = await storeMessage(response.output, data.to, data.from);
+};
 
 const io = socket(server, {
   cors: {
-    origin: "*",//"http://localhost:3005",
+    origin: "*", //"http://localhost:3005",
     credentials: true,
   },
 });
@@ -75,38 +95,37 @@ const io = socket(server, {
 global.onlineUsers = new Map();
 io.on("connection", async (socket) => {
   global.chatSocket = socket;
+
   socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
-  }); 
+  });
 
   socket.on("send-msg", async (data) => {
+    console.log(data);
     const sendUserSocket = onlineUsers.get(data.to);
     const fromUserSocket = onlineUsers.get(data.from);
+    const socketList = [socket, io, sendUserSocket, fromUserSocket];
     if (sendUserSocket) {
+      console.log("sendUseSocket msg-recieve conditional?");
       socket.to(sendUserSocket).emit("msg-recieve", data.msg);
     }
 
     // if AI requested
     if (data.modelOption !== 0) {
+      broadcastMsg(
+        socketList,
+        "msg-start-ai",
+        data.msg
+      );
       console.log("Send AI");
-      socket.to(sendUserSocket).emit("msg-start-ai", data.msg);
-      io.to(fromUserSocket).emit("msg-start-ai", data.msg);
       isAIProcessing = true;
       const response = await ollama.generate(data.msg);
-      socket.to(sendUserSocket).emit("msg-recieve-ai", response.output);
-      io.to(fromUserSocket).emit("msg-recieve-ai", response.output);
-
-
-      //persist
-      const Messages = require("./models/messageModel");
-      const newMsg = await Messages.create({
-        message: { text: response.output },
-        users: [data.from, data.to],
-        sender: mongoose.Types.ObjectId(process.env.AIUUID),
-      });
-      isAIProcessing = false;
-      console.log('AI Response');
+      broadcastMsg(
+        socketList,
+        "msg-recieve-ai",
+        response.output
+      );
+      const newMsg = await storeMessage(response.output, data.to, data.from);
     }
   });
-
 });
