7b6d6d492a961f52b0029837fc76fe0a0fb32c16
Patrick Murphy <patrickmurphydev@gmail.com>
2024-08-12 03:13:56 -0700

    update state details

--- public/src/components/IslandSketch/FiniteStateMachine.js
+++ public/src/components/IslandSketch/FiniteStateMachine.js
@@ -24,82 +24,98 @@ export const stateDetails = {
   SLEEPING: {
     name: states.SLEEPING,
     duration: { default: 480, min: 120, max: 600 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.WAKING
   },
   WAKING: {
     name: states.WAKING,
     duration: { default: 5, min: 1, max: 10 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.SHOWERING
   },
   SHOWERING: {
     name: states.SHOWERING,
     duration: { default: 10, min: 5, max: 15 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.CHANGING_CLOTHES
   },
   CHANGING_CLOTHES: {
     name: states.CHANGING_CLOTHES,
     duration: { default: 5, min: 2, max: 10 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.DRINKING_COFFEE
   },
   DRINKING_COFFEE: {
     name: states.DRINKING_COFFEE,
     duration: { default: 10, min: 5, max: 15 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.EATING_BREAKFAST
   },
   EATING_BREAKFAST: {
     name: states.EATING_BREAKFAST,
     duration: { default: 20, min: 10, max: 30 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.LEAVING_FOR_WORK
   },
   LEAVING_FOR_WORK: {
     name: states.LEAVING_FOR_WORK,
     duration: { default: 5, min: 2, max: 10 },
-    moveDetails: { requiresMove: true, isTraveling: true }
+    moveDetails: { requiresMove: true, isTraveling: true },
+    nextState: states.WALKING_TO_WORK
   },
   WALKING_TO_WORK: {
     name: states.WALKING_TO_WORK,
     duration: { default: 15, min: 10, max: 20 },
-    moveDetails: { requiresMove: true, isTraveling: true }
+    moveDetails: { requiresMove: true, isTraveling: true },
+    nextState: states.ARRIVING_AT_WORK
   },
   ARRIVING_AT_WORK: {
     name: states.ARRIVING_AT_WORK,
     duration: { default: 5, min: 2, max: 10 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.WORKING
   },
   WORKING: {
     name: states.WORKING,
     duration: { default: 120, min: 60, max: 240 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.TAKING_BREAK // Can vary based on conditions
   },
   TAKING_BREAK: {
     name: states.TAKING_BREAK,
     duration: { default: 15, min: 5, max: 30 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.WORKING // After break, back to work
   },
   LUNCH: {
     name: states.LUNCH,
     duration: { default: 30, min: 15, max: 60 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.WORKING // After lunch, back to work
   },
   GOING_HOME: {
     name: states.GOING_HOME,
     duration: { default: 15, min: 10, max: 20 },
-    moveDetails: { requiresMove: true, isTraveling: true }
+    moveDetails: { requiresMove: true, isTraveling: true },
+    nextState: states.EATING_DINNER
   },
   EATING_DINNER: {
     name: states.EATING_DINNER,
     duration: { default: 30, min: 20, max: 60 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.WATCHING_TV
   },
   WATCHING_TV: {
     name: states.WATCHING_TV,
     duration: { default: 120, min: 60, max: 180 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.GOING_TO_BED
   },
   GOING_TO_BED: {
     name: states.GOING_TO_BED,
     duration: { default: 10, min: 5, max: 20 },
-    moveDetails: { requiresMove: false, isTraveling: false }
+    moveDetails: { requiresMove: false, isTraveling: false },
+    nextState: states.SLEEPING
   }
 };
 
@@ -113,7 +129,7 @@ export class FiniteStateMachine extends EventEmitter {
   }
 
   handleTimeUpdate(minElapsed, date) {
-    const currentStateDetails = stateDetails[this.currentState];
+    const currentStateDetails = stateDetails[this.currentState.toUpperCase()];
     this.timeElapsed += minElapsed;
 
     if (this.timeElapsed >= (currentStateDetails.duration.default || 120 )){
@@ -137,38 +153,13 @@ export class FiniteStateMachine extends EventEmitter {
     this.on("stateUpdate", listener);
     //callback(this.currentState);
   }
-
+  
   autoTransition() {
-    switch (this.currState) {
-      case states.SLEEPING:
-        this.transitionToNextState(states.WAKING);
-        break;
-      case states.WAKING:
-        this.transitionToNextState(states.SHOWERING);
-        break;
-      case states.SHOWERING:
-        this.transitionToNextState(states.CHANGING_CLOTHES);
-        break;
-      case states.CHANGING_CLOTHES:
-        this.transitionToNextState(states.DRINKING_COFFEE);
-        break;
-      case states.DRINKING_COFFEE:
-        this.transitionToNextState(states.EATING_BREAKFAST);
-        break;
-      case states.EATING_BREAKFAST:
-        this.transitionToNextState(states.LEAVING_FOR_WORK);
-        break;
-      case states.LEAVING_FOR_WORK:
-        this.transitionToNextState(states.WALKING_TO_WORK);
-        break;
-      case states.WALKING_TO_WORK:
-        this.transitionToNextState(states.ARRIVING_AT_WORK);
-        break;
-      case states.ARRIVING_AT_WORK:
-        this.work_breaks = 0;
-        this.transitionToNextState(states.WORKING);
-        break;
-      case states.WORKING:
+    const currentStateDetails = stateDetails[this.currentState.toUpperCase()];
+    const nextState = currentStateDetails.nextState;
+  
+    if (nextState) {
+      if(this.currentState === states.WORKING){
         if (
           this.work_breaks === 0 ||
           (this.work_lunch > 0 && this.work_breaks === 1)
@@ -181,30 +172,19 @@ export class FiniteStateMachine extends EventEmitter {
           this.transitionToNextState(states.LUNCH);
         } else if (this.work_breaks >= 2) {
           this.transitionToNextState(states.GOING_HOME);
-        }
-        break;
-      case states.TAKING_BREAK:
+        }  
+      }else if(this.currentState === states.ARRIVING_AT_WORK) {
+        this.work_breaks = 0;
+        this.work_lunch = 0;
         this.transitionToNextState(states.WORKING);
-        break;
-      case states.LUNCH:
+      }else if(this.currentState === states.LUNCH) {
         this.work_lunch++;
         this.transitionToNextState(states.WORKING);
-        break;
-      case states.GOING_HOME:
-        this.transitionToNextState(states.EATING_DINNER);
-        break;
-      case states.EATING_DINNER:
-        this.transitionToNextState(states.WATCHING_TV);
-        break;
-      case states.WATCHING_TV:
-        this.transitionToNextState(states.GOING_TO_BED);
-        break;
-      case states.GOING_TO_BED:
-        this.transitionToNextState(states.SLEEPING);
-        break;
-      default:
-        console.error(`Invalid state: ${this.currState}`);
-        break;
+      }else{
+        this.transitionToNextState(nextState);
+      }
+    } else {
+      console.error(`No next state defined for ${this.currentState}`);
     }
   }
 
