0353a723e82503206659f6b3fef5bf3923348755
Patrick Murphy <patrickmurphydev@gmail.com>
2024-08-20 05:07:23 -0700

    fix relationships react  null

--- public/src/components/CharacterRelationships.jsx
+++ public/src/components/CharacterRelationships.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const CharacterRelationships = ({ relationships }) => {
   const renderRelationships = (type) => {
-    return relationships[type].map((person, index) => (
-      <li key={index}>{person}</li>
-    ));
+    //return relationships[type].map((person, index) => (
+    //<li key={index}>{person}</li>
+    return <li>TempEmpty</li>;
+    //));
   };
 
-  return (
-    <div className="character-relationships">
-      <h2>Relationships</h2>
-      <div className="relationship-category">
-        <h3>Friends</h3>
-        <ul>{renderRelationships('friends')}</ul>
+  if (relationships) {
+    return (
+      <div className="character-relationships">
+        <h2>Relationships</h2>
+        <div className="relationship-category">
+          <h3>Friends</h3>
+          <ul>{renderRelationships("friends")}</ul>
+        </div>
+        <div className="relationship-category">
+          <h3>Disliked People</h3>
+          <ul>{renderRelationships("disliked")}</ul>
+        </div>
+        <div className="relationship-category">
+          <h3>Romantic Feelings</h3>
+          <ul>{renderRelationships("romantic")}</ul>
+        </div>
+        <div className="relationship-category">
+          <h3>Romantic History</h3>
+          <ul>{renderRelationships("romanticHistory")}</ul>
+        </div>
       </div>
-      <div className="relationship-category">
-        <h3>Disliked People</h3>
-        <ul>{renderRelationships('disliked')}</ul>
-      </div>
-      <div className="relationship-category">
-        <h3>Romantic Feelings</h3>
-        <ul>{renderRelationships('romantic')}</ul>
-      </div>
-      <div className="relationship-category">
-        <h3>Romantic History</h3>
-        <ul>{renderRelationships('romanticHistory')}</ul>
-      </div>
-    </div>
-  );
+    );
+  }else{
+    return (<sup style={{color:'#777'}}>No Relationships</sup>)
+  }
 };
 
 export default CharacterRelationships;
