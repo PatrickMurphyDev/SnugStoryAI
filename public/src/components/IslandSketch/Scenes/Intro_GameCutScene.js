c911f1a1c3c4ae49dc47ea723f86f252843b03f9
Patrick Murphy <patrickmurphydev@gmail.com>
2024-09-06 16:30:50 -0700

    add new story changes, add wip helper function to create slides but it doesn't work with imgs.

--- public/src/components/IslandSketch/Scenes/Intro_GameCutScene.js
+++ public/src/components/IslandSketch/Scenes/Intro_GameCutScene.js
@@ -1,6 +1,39 @@
 // GameCutScene.js
 import { GameCutScene } from "./GameCutScene";
+
 const slideTypesEnum = {IMG:0,TXT:1,IMGTXT:2,SMLIMGTXT:3};
+const createSlideObjFn = (that, slideType, txt, img, choices)=>{
+  if(!parseInt(slideType)){
+    // first param is text
+    if(img){
+      choices = img;
+    }
+    img = txt;
+    txt = slideType;
+    slideType = slideTypesEnum.IMGTXT;
+  }
+
+  choices = choices || [
+    {
+      text: "Previous",
+      onClick: () => {
+        that.previousSlide();
+      },
+    },
+    {
+      text: "Next",
+      onClick: () => {
+        that.nextSlide();
+      },
+    },
+  ];
+
+  return {
+    slideType: slideType,
+    text: txt,
+    choices: choices,
+  };
+};
 
 export class Intro_GameCutScene extends GameCutScene {
   constructor() {
@@ -16,12 +49,10 @@ export class Intro_GameCutScene extends GameCutScene {
     ]),
       {
         imagePath: "images/cutscenes/intro/0.png",
-        text: "*Man I wish I had family here to support me like everyone else, I thought I would at least have a group of close friends by now\n\nMaybe I should have spent less time studying and been more social.\n\nSure I am graduating almost a year early but am I the only one without an internship or plans afer graduation...*",
-        textArray: [
-          "*Man I wish I had family here to support me like everyone else, I thought I would at least have a group of close friends by now*",
-          "*Maybe I should have spent less time studying and been more social.*",
-          "*Sure I am graduating almost a year early but am I the only one without an internship or plans afer graduation...*",
-        ],
+        text: `*ughh* I wish I had family here to support me like everyone else does,
+         my post-18 plan notebook assumed I would have a group of close friends by now
+        \nMaybe I should have spent less time studying and been more social.
+        \nSure I am graduating almost a year early but I feel like the only one without an internship or job lined up...*`,
         choices: [
           {
             text: "Next",
@@ -63,6 +94,28 @@ export class Intro_GameCutScene extends GameCutScene {
         imagePath: "images/cutscenes/intro/1.png",
         text: `“However, you were listed in his Last Will & Testament. His estate includes his home and much more is entailed.
       \n\nIf you have any questions please come to the open house next week.”`,
+        choices: [
+          {
+            text: "Previous",
+            onClick: () => {
+              this.previousSlide();
+            },
+          },
+          {
+            text: "Next",
+            onClick: () => {
+              this.nextSlide();
+            },
+          },
+        ],
+      },{
+        slideType: 1,
+        text: `I.. Don't feel so good....
+
+        *vision blurs*
+        
+        
+        2016 - 15 Years Old`,
         choices: [
           {
             text: "Previous",
@@ -129,7 +182,7 @@ export class Intro_GameCutScene extends GameCutScene {
         imagePath: "images/cutscenes/intro/4.png",
         text: `I turned 18 and things finally felt like they were going to go my way...
 
-        I got a full ride Scholarship to a good University for my academics...`,
+        I got a full ride Scholarship for my academics to Penn State University...`,
         choices: [
           {
             text: "Previous",
