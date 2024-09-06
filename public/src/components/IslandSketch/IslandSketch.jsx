0a00a85ac43e7a2efb865129c25523a2dcebc1c4
Patrick Murphy <patrickmurphydev@gmail.com>
2024-09-06 01:59:26 -0700

    fix img load for cut scene

--- public/src/components/IslandSketch/IslandSketch.jsx
+++ public/src/components/IslandSketch/IslandSketch.jsx
@@ -2,34 +2,36 @@
 import React, { useState } from 'react';
 import Sketch from 'react-p5';
 import { GameMapScene } from './Entities/GameMapScene';
-import { GameMenuScene } from './Entities/GameMenuScene';
-import { GameCutScene } from './Entities/GameCutScene';
+import { Main_GameMenuScene } from './Entities/Main_GameMenuScene';
+import { Intro_GameCutScene } from './Entities/Intro_GameCutScene';
 
 const IslandSketch = ({ onCharacterSelect, onPropertySelect, charList, setCharList, sizeVector = { x: 800, y: 600 } }) => {
   const [currentSceneIndex, setCurrentSceneIndex] = useState(0);
 
   const scenes = [
-    new GameMenuScene(),
-    new GameCutScene(),
+    new Main_GameMenuScene(IslandSketch),
+    new Intro_GameCutScene(),
     new GameMapScene(onCharacterSelect, onPropertySelect, charList, setCharList, sizeVector),
   ];
-
-  const nextScene = () => {
-    setCurrentSceneIndex((prevIndex) => (prevIndex + 1) % scenes.length);
-  };
-
-  const prevScene = () => {
-    setCurrentSceneIndex((prevIndex) => (prevIndex - 1 + scenes.length) % scenes.length);
-  };
 
   const goToScene = (index) => {
     if (index >= 0 && index < scenes.length) {
       setCurrentSceneIndex(index);
+    }else{
+      console.error('Non-valid scene index '+ index);
     }
   };
 
-  const preload = (p5) => scenes[currentSceneIndex].preload(p5);
-  const setup = (p5, canvasParentRef) => scenes[currentSceneIndex].setup(p5, canvasParentRef);
+  const nextScene = () => {
+    goToScene((currentSceneIndex + 1) % scenes.length);
+  };
+
+  const prevScene = () => {
+    goToScene((currentSceneIndex - 1 + scenes.length) % scenes.length);
+  };
+
+  const preload = (p5) => scenes.forEach((val,i)=>{scenes[i].preload(p5);});
+  const setup = (p5, canvasParentRef) => p5.createCanvas(800, 600).parent(canvasParentRef);
   const draw = (p5) => scenes[currentSceneIndex].draw(p5);
 
   return (
