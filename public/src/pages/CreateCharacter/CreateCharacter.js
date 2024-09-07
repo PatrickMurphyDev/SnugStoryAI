a4c53ee2012db3c36bfe02337d1cea7165faeabe
Patrick Murphy <patrickmurphydev@gmail.com>
2024-09-07 04:06:42 -0700

    forgot axios import

--- public/src/pages/CreateCharacter/CreateCharacter.js
+++ public/src/pages/CreateCharacter/CreateCharacter.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import BasicDetailsStep from "./subpages/BasicDetailsStep";
 import MentalDetailsStep from "./subpages/MentalDetailsStep";
 import PhysicalAppearanceStep from "./subpages/PhysicalAppearanceStep";
