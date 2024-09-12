ae20cf3ac6bee1f4661b1f09e878b420cfd62a78
Patrick Murphy <patrickmurphydev@gmail.com>
2024-09-12 06:55:03 -0700

    change ui delay

--- public/src/components/IslandSketch/Scenes/GameScene.js
+++ public/src/components/IslandSketch/Scenes/GameScene.js
@@ -4,7 +4,7 @@ export class GameScene {
       this.name = name; // Scene name
       
       this.lastUIActionFrame = -1;
-      this.UIActionCooldownFrames = 60;
+      this.UIActionCooldownFrames = 15;
     }
 
     doUIAction(currFrame, action){
