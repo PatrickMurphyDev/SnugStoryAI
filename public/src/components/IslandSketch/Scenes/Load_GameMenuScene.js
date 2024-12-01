8270ee3cd79800dcd1b6ff051d95a969d7d29d47
Patrick Murphy <patrickmurphydev@gmail.com>
2024-12-01 07:58:28 -0800

    Load GameScene relative button positioning New Game, Load Gme

--- public/src/components/IslandSketch/Scenes/Load_GameMenuScene.js
+++ public/src/components/IslandSketch/Scenes/Load_GameMenuScene.js
@@ -1,23 +1,26 @@
 import {GameMenuScene} from "./GameMenuScene.js";
 
+// TODO: make btnSizes a global constant for easy modification
+const btnSizes = { widthPct: 210/800.00,"heightPct": 45/600.0,}
+
 export class Load_GameMenuScene extends GameMenuScene {
   constructor(parentSetSceneFN, newGameScene = 1, loadGameScene = 1, backScene = 0) {
     super("images/Load_GameMenuScene.png", [
       {
-        x: 123,
-        y: 193,
-        width: 185,
-        height: 42,
+        x: 123 * 1.66,
+        y: 245 * 1.66,
+        width: btnSizes.widthPct*1000,
+        height: btnSizes.heightPct*800,
         text: "New Game",
         onClick: () => {
           parentSetSceneFN(newGameScene);
         },
       },
       {
-        x: 123,
-        y: 240,
-        width: 185,
-        height: 42,
+        x: 123 * 1.66,
+        y: 305 * 1.66,
+        width: btnSizes.widthPct*1000,
+        height: btnSizes.heightPct*800,
         text: "Load Game",
         onClick: () => {
           parentSetSceneFN(loadGameScene);
