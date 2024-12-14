3030765feb9db9cfcbf99af9e28f697cb5475374
Patrick Murphy <patrickmurphydev@gmail.com>
2024-12-14 04:33:10 -0800

    add store fn to worldactionlog

--- server/controllers/worldActionLog.js
+++ server/controllers/worldActionLog.js
@@ -10,6 +10,12 @@ class WorldActionLog {
   async addAction(action) {
     this.log.push({action});
   }
+
+  // save game state to db by saveGameID, gameState is JSON string
+  async storeGameActionLog(gameAction) {
+    const gameActionLog = require("../models/WorldActionLogModel");
+    let saveRow = await gameActionLog.create(gameAction);
+  }
 }
 
 module.exports = WorldActionLog;
