795d0c14b556862c939b4bd734464787b2b238ad
Patrick Murphy <patrickmurphydev@gmail.com>
2025-02-04 08:06:32 -0800

    change covo manager to use mongoose

--- server/controllers/ConversationManager.js
+++ server/controllers/ConversationManager.js
@@ -2,102 +2,96 @@ const ConversationModel = require('../models/v3/Interaction/ConversationModel');
 
 class ConversationManager {
   constructor() {
-    this.ConversationHistory = [];
-    this.ConversationHistoryParticipantsMap = new Map();
     this.currentConversation = null;
   }
-  getPastConversations(npcKeys, convoReturnLimit = Infinity, exclusive = false, convoMsgReturnLimit = Infinity) {
-    const npcSet = new Set(npcKeys);
-    const filteredConversations = this.ConversationHistory.filter(conversation => {
-      const participants = this.ConversationHistoryParticipantsMap.get(conversation.id);
-      if (exclusive) {
-        return npcSet.size === participants.length && participants.every(npc => npcSet.has(npc));
-      } else {
-        return participants.some(npc => npcSet.has(npc));
-      }
+
+  
+// Is Used in SocketManager.js
+  async addConversation(conversation, participants) {
+    const newConversation = new ConversationModel.Conversation({
+      ...conversation,
+      participants,
+      messages: conversation.messages || []
     });
 
-    return filteredConversations
-      .sort((a, b) => b.creationTime - a.creationTime)
-      .slice(0, convoReturnLimit)
-      .map(conversation => ({
-        ...conversation,
-        messages: conversation.messages.slice(-convoMsgReturnLimit)
-      }));
+    const savedConversation = await newConversation.save();
+    return savedConversation._id;
   }
-
-
-  addConversation(conversation, participants) {
-    const conversationId = this.ConversationHistory.length;
-    const newConversation = {
-      ...conversation,
-      index: conversationId,
-      messages: conversation.messages || []
-    };
-
-    this.ConversationHistory.push(newConversation);
-    this.ConversationHistoryParticipantsMap.set(conversationId, participants);
-    ConversationModel.create(newConversation);
-    //console.log(`Added new conversation: ${conversationId}`);
-    return conversationId;
+  
+  async setCurrentConversation(conversationId) {
+    this.currentConversation = await ConversationModel.Conversation.findById(conversationId);
   }
-
   getCurrentConversation() {
     return this.currentConversation;
   }
 
-  setCurrentConversation(conversationId) {
-    this.currentConversation = this.getConversationByIndex(conversationId);
-  }
+  
 
-  addMessageToConversation(conversationId, message) {
-    const conversation = this.getConversationByIndex(conversationId);
-    if (conversation) {
-      conversation.messages.push({
-        ...message,
-        timestamp: Date.now()
-      });
-    }
-  }
-  getConversationByIndex(index) {
-    if (index >= 0 && index < this.ConversationHistory.length) {
-      return this.ConversationHistory[index];
-    }
-    return null;
-  }
-
-  getConversationIndexesInvolving(npcKeys) {
-    const npcSet = new Set(npcKeys);
-    return this.ConversationHistory.reduce((indexes, conversation, index) => {
-      const participants = this.ConversationHistoryParticipantsMap.get(conversation.id);
-      if (participants.some(npc => npcSet.has(npc))) {
-        indexes.push(index);
+  async addMessageToConversation(conversationId, message) {
+    await ConversationModel.Conversation.findByIdAndUpdate(
+      conversationId,
+      { 
+        $push: { 
+          messages: {
+            ...message,
+            timestamp: Date.now()
+          }
+        }
       }
-      return indexes;
-    }, []);
-  }
-
-  removeConversation(conversationId) {
-    const index = this.ConversationHistory.findIndex(c => c.id === conversationId);
-    if (index !== -1) {
-      this.ConversationHistory.splice(index, 1);
-      this.ConversationHistoryParticipantsMap.delete(conversationId);
-      return true;
-    }
-    return false;
-  }
-
-  searchConversationsByKeywords(keywords) {
-    const keywordSet = new Set(keywords.map(k => k.toLowerCase()));
-    return this.ConversationHistory.filter(conversation =>
-      conversation.messages.some(message =>
-        message.text.toLowerCase().split(' ').some(word => keywordSet.has(word))
-      )
     );
   }
+/*
 
-  getConversationMessages(conversationId, page = 1, pageSize = 20) {
-    const conversation = this.getConversationByIndex(conversationId);
+
+  async getConversationById(id) {
+    return await ConversationModel.Conversation.findById(id);
+  }
+
+  async getConversationIndexesInvolving(npcKeys) {
+    const conversations = await ConversationModel.Conversation.find(
+      { participants: { $in: npcKeys } },
+      '_id'
+    );
+    return conversations.map(conv => conv._id);
+  }
+
+  /*
+   async getPastConversations(npcKeys, convoReturnLimit = Infinity, exclusive = false, convoMsgReturnLimit = Infinity) {
+    let query;
+    if (exclusive) {
+      query = { participants: { $all: npcKeys, $size: npcKeys.length } };
+    } else {
+      query = { participants: { $in: npcKeys } };
+    }
+
+    const conversations = await ConversationModel.Conversation
+      .find(query)
+      .sort({ creationTime: -1 })
+      .limit(convoReturnLimit)
+      .lean();
+
+    return conversations.map(conversation => ({
+      ...conversation,
+      messages: conversation.messages.slice(-convoMsgReturnLimit)
+    }));
+  }
+
+/*
+  async removeConversation(conversationId) {
+    const result = await ConversationModel.Conversation.findByIdAndDelete(conversationId);
+    return !!result;
+  }
+/*
+  async searchConversationsByKeywords(keywords) {
+    const keywordRegex = keywords.map(k => new RegExp(k, 'i'));
+    return await ConversationModel.Conversation.find({
+      'messages.content': { $in: keywordRegex }
+    });
+  }
+
+/*
+  async getConversationMessages(conversationId, page = 1, pageSize = 20) {
+    const conversation = await ConversationModel.Conversation.findById(conversationId);
     if (!conversation) return [];
 
     const startIndex = (page - 1) * pageSize;
@@ -108,11 +102,12 @@ class ConversationManager {
       .slice(startIndex, endIndex);
   }
 
-  getParticipantCount(conversationId) {
-    const participants = this.ConversationHistoryParticipantsMap.get(conversationId);
-    return participants ? participants.length : 0;
-  }
+  async getParticipantCount(conversationId) {
+    const conversation = await ConversationModel.Conversation.findById(conversationId);
+    return conversation ? conversation.participants.length : 0;
+  }*/
 }
 
 module.exports = ConversationManager;
+
 
